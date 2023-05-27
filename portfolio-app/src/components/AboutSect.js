3e229dd2f8e32340ff7b1a70068336faa1d69f9f
Clayton Van Imschoot <clayton.vanimschoot@gmail.com>
2023-05-26 22:17:57 -0600

    Fix and finish text for My Journey sect

--- portfolio-app/src/components/AboutSect.js
+++ portfolio-app/src/components/AboutSect.js
@@ -2,20 +2,20 @@ export default function AboutSect() {
   return (
     <div className="bg-black w-full h-[800px] p-36">
       <div className="text-6xl text-[#8c8792]">
-        About Me
+        My Journey
       </div>
       <div className="text-3xl pt-8 flex">
         <div className="w-1/4 pt-6 pb-6 pr-6">
-          I have a big love for video games. A lifelong dream of mine was to be able to create a video game of my own, and I was able to accomplish this from my game programming stream at NAIT.
+          I started my developer journey during my time at Macewan, where I took some basic HTML and CSS classes. Alongside of this I taught myself how to build and maintain PCs, which acted as a secondary source of income alongside my main work.
         </div>
         <div className="w-1/4 p-6">
-          LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM
+          My love for video games carried me into getting my diploma in game programming, which I got from Northern Alberta Institue of Technology. This acted as my first real exposure to learning a language that I could be fully proficient in, allowing me to create complete and publishable projects.
         </div>
         <div className="w-1/4 p-6">
-          LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM
+          After discovering my passion for programming, and after some advice on what to learn next, I moved onto Lighthouse Labs. Here I learned an abudance of languages, frameworks, and tech stacks to make me a proficient junior full stack developer
         </div>
         <div className="w-1/4 p-6">
-          LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM
+          My current place on this journey is why I hope this portfolio will interest you. I'm looking to find a team to work with that will apply the skills that I've developed and further expand my knowledge within the industry and your company.
         </div>
       </div>
     </div>
