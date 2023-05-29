9f953dd1ab654efcdd6cc03f4af84028d09f06a1
Clayton Van Imschoot <clayton.vanimschoot@gmail.com>
2023-05-29 16:10:27 -0600

    styling change. WIP

--- portfolio-app/src/components/ExperienceSect.js
+++ portfolio-app/src/components/ExperienceSect.js
@@ -3,7 +3,10 @@ import Image from "next/image"
 
 export default function ExperienceSect() {
   return (
-    <div className="bg-black w-full h-[800px] bg-[url('../../public/beach-bgi.jpg')] bg-contain bg-no-repeat">
+    <div className="bg-black w-full h-[800px] bg-[url('../../public/background-beach.png')] px-36 py-20">
+      <div className="bg-[#2f2837] w-full h-full">
+
+      </div>
     </div>
   )
 }
