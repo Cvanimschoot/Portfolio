93586f21d6c7b29b7a9171dc8efa4c96e74fc1fe
Clayton Van Imschoot <clayton.vanimschoot@gmail.com>
2023-06-02 15:28:53 -0600

    Add mobile view for about section

--- portfolio-app/src/components/AboutSect.js
+++ portfolio-app/src/components/AboutSect.js
@@ -1,20 +1,20 @@
 export default function AboutSect() {
   return (
-    <div className="bg-black w-full h-[800px] p-24">
-      <div className="text-6xl text-[#8c8792]">
+    <div className="bg-black w-full h-[800px] p-24 max-sm:p-2 max-sm:flex max-sm:flex-col max-sm:items-center">
+      <div className="text-6xl text-[#8c8792] max-sm:text-3xl">
         My Journey
       </div>
-      <div className="text-3xl pt-8 flex">
-        <div className="w-1/4 pt-6 pb-6 pr-6">
+      <div className="text-3xl pt-8 flex max-sm:text-xl max-sm:flex-col">
+        <div className="w-1/4 pt-6 pb-6 pr-6 max-sm:w-full max-sm:p-6">
           I started my developer journey during my time at Macewan, where I took some basic HTML and CSS classes. Alongside of this I taught myself how to build and maintain PCs, which acted as a secondary source of income alongside my main work.
         </div>
-        <div className="w-1/4 p-6">
+        <div className="w-1/4 p-6 max-sm:w-full">
           My love for video games carried me into getting my diploma in game programming, which I got from Northern Alberta Institue of Technology. This acted as my first real exposure to learning a language that I could be fully proficient in, allowing me to create complete and publishable projects.
         </div>
-        <div className="w-1/4 p-6">
+        <div className="w-1/4 p-6 max-sm:w-full">
           After discovering my passion for programming, and after some advice on what to learn next, I moved onto Lighthouse Labs. Here I learned an abudance of languages, frameworks, and tech stacks to make me a proficient junior full stack developer
         </div>
-        <div className="w-1/4 p-6">
+        <div className="w-1/4 p-6 max-sm:w-full">
           My current place on this journey is why I hope this portfolio will interest you. I'm looking to find a team to work with that will apply the skills that I've developed and further expand my knowledge within the industry and your company.
         </div>
       </div>
