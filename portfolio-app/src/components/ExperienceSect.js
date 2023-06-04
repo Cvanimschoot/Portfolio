075716299106c67f97c927aae54ccf83d81bf04a
Clayton Van Imschoot <clayton.vanimschoot@gmail.com>
2023-06-03 23:55:38 -0600

    Change of web technologies flow. Remove animation

--- portfolio-app/src/components/ExperienceSect.js
+++ portfolio-app/src/components/ExperienceSect.js
@@ -1,38 +1,27 @@
 export default function ExperienceSect() {
   return (
-    <div className="bg-black w-full h-[800px] bg-[url('../../public/background-beach.png')] px-36 py-20">
-      <div className="bg-[#2f2837] w-full h-full overflow-hidden whitespace-nowrap">
-        {/* <div className="bg-white w-full h-1/4 inline-block"> */}
-        {/* <div className="h-full relative p-2 animate-scroll"> */}
-        <img className="mr-5 h-[50px]" src='csharp.svg' />
-        <img className="mr-5 h-[50px]" src='git.svg' />
-        <img className="mr-5 h-[50px]" src='html5.svg' />
-        <img className="mr-5 h-[50px]" src='javascript.svg' />
-        <img className="mr-5 h-[50px]" src='json.svg' />
-        <img className="mr-5 h-[50px]" src='nextdotjs.svg' />
-        <img className="mr-5 h-[50px]" src='nodedotjs.svg' />
-        <img className="mr-5 h-[50px]" src='npm.svg' />
-        <img className="mr-5 h-[50px]" src='postgresql.svg' />
-        <img className="mr-5 h-[50px]" src='react.svg' />
-        <img className="mr-5 h-[50px]" src='rubyonrails.svg' />
-        <img className="mr-5 h-[50px]" src='tailwindcss.svg' />
+    <>
+      <div className="bg-black w-full h-[800px] bg-[url('../../public/background-beach.png')] px-36 py-20">
+        <div className="bg-[#2f2837] w-full h-full max-lg:invisible">
+          <div className="bg-[#b1a3bc] h-1/6 w-full flex p-3">
+            <img className="mr-5 w-1/12" src='csharp.svg' />
+            <img className="mr-5 w-1/12" src='git.svg' />
+            <img className="mr-5 w-1/12" src='html5.svg' />
+            <img className="mr-5 w-1/12" src='javascript.svg' />
+            <img className="mr-5 w-1/12" src='json.svg' />
+            <img className="mr-5 w-1/12" src='nextdotjs.svg' />
+            <img className="mr-5 w-1/12" src='nodedotjs.svg' />
+            <img className="mr-5 w-1/12" src='npm.svg' />
+            <img className="mr-5 w-1/12" src='postgresql.svg' />
+            <img className="mr-5 w-1/12" src='react.svg' />
+            <img className="mr-5 w-1/12" src='rubyonrails.svg' />
+            <img className="mr-5 w-1/12" src='tailwindcss.svg' />
+          </div>
+        </div>
       </div>
-      <div className="h-full flex relative p-2 animate-scroll">
-        <img className="mr-5" src='csharp.svg' />
-        <img className="mr-5" src='git.svg' />
-        <img className="mr-5" src='html5.svg' />
-        <img className="mr-5" src='javascript.svg' />
-        <img className="mr-5" src='json.svg' />
-        <img className="mr-5" src='nextdotjs.svg' />
-        <img className="mr-5" src='nodedotjs.svg' />
-        <img className="mr-5" src='npm.svg' />
-        <img className="mr-5" src='postgresql.svg' />
-        <img className="mr-5" src='react.svg' />
-        <img className="mr-5" src='rubyonrails.svg' />
-        <img className="mr-5" src='tailwindcss.svg' />
+      <div className="bg-[#2f2837] w-full h-[800px]">
+
       </div>
-    </div>
-    //   </div>
-    // </div>
+    </>
   )
 }
