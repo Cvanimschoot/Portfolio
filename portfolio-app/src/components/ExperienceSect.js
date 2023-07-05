57630aac4bc95fa2f582e2b2a6b96c1fa749869a
Clayton Van Imschoot <clayton.vanimschoot@gmail.com>
2023-07-05 00:14:40 -0600

    Change some styling on tech stack. Add layout for experience section

--- portfolio-app/src/components/ExperienceSect.js
+++ portfolio-app/src/components/ExperienceSect.js
@@ -1,18 +1,67 @@
+import { useEffect, useRef } from 'react';
+// import { gsap } from 'gsap';
+
 export default function ExperienceSect() {
+
   return (
-    <div className="bg-[#c9c9c9] w-full h-[150px] p-2 flex">
-      <img className="mr-10" src='csharp.svg' />
-      <img className="mr-10" src='git.svg' />
-      <img className="mr-10" src='html5.svg' />
-      <img className="mr-10" src='javascript.svg' />
-      <img className="mr-10" src='json.svg' />
-      <img className="mr-10" src='nextdotjs.svg' />
-      <img className="mr-10" src='nodedotjs.svg' />
-      <img className="mr-10" src='npm.svg' />
-      <img className="mr-10" src='postgresql.svg' />
-      <img className="mr-10" src='react.svg' />
-      <img className="mr-10" src='rubyonrails.svg' />
-      <img className="mr-10" src='tailwindcss.svg' />
+
+    <div className="bg-[#c9c9c9] w-[full] h-[150px] p-2 flex justify-evenly">
+      <img className="" src='csharp.svg' />
+      <img className="" src='git.svg' />
+      <img className="" src='html5.svg' />
+      <img className="" src='javascript.svg' />
+      <img className="" src='json.svg' />
+      <img className="" src='nextdotjs.svg' />
+      <img className="" src='nodedotjs.svg' />
+      <img className="" src='npm.svg' />
+      <img className="" src='postgresql.svg' />
+      <img className="" src='react.svg' />
+      <img className="" src='rubyonrails.svg' />
+      <img className="" src='tailwindcss.svg' />
     </div>
+
   )
 }
+
+//const images = [
+  //   'csharp.svg',
+  //   'git.svg',
+  //   'html5.svg',
+  //   'javascript.svg',
+  //   'json.svg',
+  //   'nextdotjs.svg',
+  //   'nodedotjs.svg',
+  //   'npm.svg',
+  //   'postgresql.svg',
+  //   'react.svg',
+  //   'rubyonrails.svg',
+  //   'tailwindcss.svg'
+  // ];
+
+  // const containerRef = useRef(null);
+
+  // useEffect(() => {
+  //   const container = containerRef.current;
+
+  //   const slideDuration = 5; // Adjust the duration (in seconds) for each slide
+  //   const slideDelay = 1; // Adjust the delay (in seconds) between each slide
+
+  //   let currentIndex = 0;
+
+  //   const slide = () => {
+  //     const tl = gsap.timeline({ onComplete: slide });
+
+  //     tl.to(container, { duration: slideDuration, x: '-100%', ease: 'power1.inOut' })
+  //       .add(() => {
+  //         currentIndex = (currentIndex + 1) % images.length;
+  //         container.appendChild(container.firstChild);
+  //         container.style.transform = 'translateX(0)';
+  //       }, `-=${slideDuration - slideDelay}`);
+  //   };
+
+  //   const interval = setInterval(slide, (slideDuration + slideDelay) * 1000);
+
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // }, [images.length]);
--- portfolio-app/src/components/ProjectSect.js
+++ portfolio-app/src/components/ProjectSect.js
@@ -3,7 +3,10 @@ export default function ProjectSect() {
     <div className="bg-black w-full h-auto">
       <div className="h-[800px] flex">
         <div className="w-1/3 px-24 py-10">
-          <div className="bg-[#2f2837] h-5/6 rounded-lg"></div>
+          <div className="bg-[#2f2837] h-5/6 rounded-lg p-1 flex flex-col items-center">
+            <img className="mb-2 rounded-lg" src='ExpatHomePage.gif' />
+            <p className="text-2xl">Expat-ations</p>
+          </div>
         </div>
         <div className="w-1/3 px-24 py-10">
           <div className="h-1/6"></div>
