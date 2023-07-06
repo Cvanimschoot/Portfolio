3d7f583f670a7b9388b0dcd910973bf791a43f2c
Clayton Van Imschoot <clayton.vanimschoot@gmail.com>
2023-07-05 23:36:16 -0600

    Added photos for projects and descriptions

--- portfolio-app/src/components/ProjectSect.js
+++ portfolio-app/src/components/ProjectSect.js
@@ -6,14 +6,35 @@ export default function ProjectSect() {
           <div className="bg-[#2f2837] h-5/6 rounded-lg p-1 flex flex-col items-center">
             <img className="mb-2 rounded-lg" src='ExpatHomePage.gif' />
             <p className="text-2xl">Expat-ations</p>
+            <div className="mt-6 px-6">
+              <p className="py-2">A city comparison app that allows user to explore aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa</p>
+              <p className="py-2">A city comparison app that allows user to explore aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa</p>
+              <p className="py-2">A city comparison app that allows user to explore aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa</p>
+            </div>
           </div>
         </div>
         <div className="w-1/3 px-24 py-10">
           <div className="h-1/6"></div>
-          <div className="bg-[#2f2837] h-5/6 rounded-lg"></div>
+          <div className="bg-[#2f2837] h-5/6 rounded-lg p-1 flex flex-col items-center">
+            <img className="mb-2 rounded-lg" src='MapProject.png' />
+            <p className="text-2xl">Map & Marker Project</p>
+            <div className="mt-6 px-6">
+              <p className="py-2">A city comparison app that allows user to explore aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa</p>
+              <p className="py-2">A city comparison app that allows user to explore aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa</p>
+              <p className="py-2">A city comparison app that allows user to explore aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa</p>
+            </div>
+          </div>
         </div>
         <div className="w-1/3 px-24 py-10">
-          <div className="bg-[#2f2837] h-5/6 rounded-lg"></div>
+          <div className="bg-[#2f2837] h-5/6 rounded-lg p-1 flex flex-col items-center">
+            <img className="mb-2 rounded-lg" src='SnowWayOut.png' />
+            <p className="text-2xl">Snow Way Out</p>
+            <div className="mt-6 px-6">
+              <p className="py-2">A city comparison app that allows user to explore aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa</p>
+              <p className="py-2">A city comparison app that allows user to explore aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa</p>
+              <p className="py-2">A city comparison app that allows user to explore aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
